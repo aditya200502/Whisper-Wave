435f22586250dfb20682d739e6eee8dedb1c3a02
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2024-06-16 09:43:50 +0530

    Home page is initiated using react-lazy

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import React, { lazy } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+const Home = lazy(() => import('./pages/Home') )
 
 function App() {
-  return(
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<h1>Home</h1>}/>
-    <Route path='about' element={<h1>About</h1>}/>
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
