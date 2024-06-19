2fecffee390486ce41770ebe3877e3a3108a0e18
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2024-06-19 18:31:53 +0530

    Login Page is started

--- client/vite-project/src/pages/Login.jsx
+++ client/vite-project/src/pages/Login.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Container, Paper } from "@mui/material"
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <Container component={"main"} maxWidth="sx">
+
+            <Paper elevation={3}
+                sx={{
+                    padding: 4,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}>
+
+            </Paper>
+        </Container>
+    )
 }
 
 export default Login
