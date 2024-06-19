f688b2ab9530289c12a4253b41148299f3690393
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2024-06-19 18:21:42 +0530

    4 pages is initialized app,chat,groups and login

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -3,13 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 const Home = lazy(() => import('./pages/Home') )
+const Login = lazy(() => import('./pages/Login'))
+const Chats = lazy(() => import ('./pages/Chat'))
+const Groups = lazy(() => import('./pages/Groups'))
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='about' element={<h1>About</h1>} />
+        <Route path='/chats/:chatId' element={<Chats/>} />
+        <Route path='/groups' element={<Groups/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </BrowserRouter>
   )
