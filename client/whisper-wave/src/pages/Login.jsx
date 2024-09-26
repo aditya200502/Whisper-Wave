a2866325796aa662850a7e6e29e2d1181627a48a
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2024-09-26 21:13:15 +0530

    LogIn UI setup is done

--- client/whisper-wave/src/pages/Login.jsx
+++ client/whisper-wave/src/pages/Login.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
-import { Container, Paper } from "@mui/material"
+import { Button, Container, Paper, TextField, Typography } from "@mui/material"
+
+
 
 function Login() {
 
     const [isLogin, setIsLogin] = useState("true");
+    const ToggleLogin = () => setIsLogin(false);
     return (
-        <Container component={"main"} maxWidth="sx">
+        <Container component={"main"} maxWidth="xs" sx={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
 
             <Paper elevation={3}
                 sx={{
@@ -19,10 +27,21 @@ function Login() {
                     isLogin ? (<>
 
                         <Typography variant="h5">Login</Typography>
+                        <form style={{
+                            width: "100%",
+                            marginTop: "1rem"
+                        }}>
+                            <TextField required fullWidth label="UserName" margin='normal' variant='outlined' />
+                            <TextField required fullWidth label="PassWord" type='password' margin='normal' variant='outlined' />
+                            <Button sx={{ marginTop: "1rem" }} fullWidth variant='contained' color='primary' type='submit'>Login</Button>
+                            <Typography textAlign={'center'} m={'1rem'}>Or</Typography>
+
+                            <Button fullWidth variant='text' onClick={ToggleLogin}>SignIn</Button>
+                        </form>
 
                     </>)
-                        : <span>Register</span>
-                }
+                        : (<span>Register</span>
+                        )}
 
             </Paper>
         </Container>
