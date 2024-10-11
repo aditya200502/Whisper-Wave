f823f4a2cd019ec31ec7794fb908b405ac84ab01
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2024-10-11 23:30:36 +0530

    avaatar handler is done. Preventing default submit of login and signup page is done

--- client/whisper-wave/src/pages/Login.jsx
+++ client/whisper-wave/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Avatar, Button, Container, IconButton, Paper, Stack, TextField, Typography } from "@mui/material"
 import { CameraAlt as CameraAltIcon } from '@mui/icons-material'
 import { VisuallyHiddenInput } from '../components/styles/StyledComponent';
-import { useInputValidation, useStrongPassword } from '6pp'
+import { useFileHandler, useInputValidation, useStrongPassword } from '6pp'
 import { UserNameValidator } from '../utils/validator';
 
 function Login() {
@@ -15,6 +15,15 @@ function Login() {
     const bio = useInputValidation("");
     const username = useInputValidation("", UserNameValidator);
     const password = useStrongPassword();
+
+    const avatar = useFileHandler("single")
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+    }
+    const handleSignUp = (e) => {
+        e.preventDefault();
+    }
     return (
         <Container component={"main"} maxWidth="xs" sx={{
             height: '100vh',
@@ -39,7 +48,8 @@ function Login() {
                         <form style={{
                             width: "100%",
                             marginTop: "1rem"
-                        }}>
+                        }}
+                            onSubmit={handleLogin}>
                             <TextField required fullWidth label="UserName" margin='normal' variant='outlined' value={username.value} onChange={username.changeHandler} />
                             <TextField required fullWidth label="Password" type='password' margin='normal' variant='outlined' value={password.value} onChange={password.changeHandler} />
                             <Button sx={{ marginTop: "1rem" }} fullWidth variant='contained' color='primary' type='submit'>Login</Button>
@@ -55,14 +65,16 @@ function Login() {
                             <form style={{
                                 width: "100%",
                                 marginTop: "1rem"
-                            }}>
+                            }}
+                                onSubmit={handleSignUp}>
 
                                 <Stack position={"relative"} width={"10rem"} margin={"auto"}>
                                     <Avatar sx={{
                                         width: "10rem",
                                         height: "10rem",
                                         objectFit: "contain"
-                                    }} />
+                                    }}
+                                        src={avatar.preview} />
 
                                     <IconButton sx={{
                                         position: "absolute",
@@ -79,10 +91,16 @@ function Login() {
                                     >
                                         <>
                                             <CameraAltIcon />
-                                            <VisuallyHiddenInput type='file' />
+                                            <VisuallyHiddenInput type='file' onChange={avatar.changeHandler} />
                                         </>
                                     </IconButton>
                                 </Stack>
+
+                                {avatar.error && (
+                                    <Typography m={"1rem"} width={"fit-content"} display={"block"} color="error" variant='caption'>
+                                        {avatar.error}
+                                    </Typography>
+                                )}
 
                                 <TextField required fullWidth label="Name" margin='normal' variant='outlined' value={name.value} onChange={name.changeHandler} />
                                 <TextField required fullWidth label="Bio" margin='normal' variant='outlined' value={bio.value} onChange={bio.changeHandler} />
