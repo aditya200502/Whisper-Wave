0ffbc3673fdc095d07e531274c638dddf86a3e36
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2024-10-13 22:38:05 +0530

    Home Redirected to Login page is done if not loggedin

--- client/whisper-wave/src/App.jsx
+++ client/whisper-wave/src/App.jsx
@@ -1,20 +1,25 @@
 import React, { lazy } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from './components/auth/ProtectRoute';
 
 
-const Home = lazy(() => import('./pages/Home') )
+const Home = lazy(() => import('./pages/Home'))
 const Login = lazy(() => import('./pages/Login'))
-const Chats = lazy(() => import ('./pages/Chat'))
+const Chats = lazy(() => import('./pages/Chat'))
 const Groups = lazy(() => import('./pages/Groups'))
+
+let user = true;
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/chats/:chatId' element={<Chats/>} />
-        <Route path='/groups' element={<Groups/>} />
-        <Route path='/login' element={<Login/>} />
+        <Route path='/' element={<ProtectRoute user={user}>
+          <Home />
+        </ProtectRoute>} />
+        <Route path='/chats/:chatId' element={<Chats />} />
+        <Route path='/groups' element={<Groups />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
