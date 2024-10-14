2b17b38ed4a7f0f45fa06ac06feb09f952ad347d
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2024-10-15 00:14:48 +0530

    NotFound is added for other pages which are not in routes

--- client/whisper-wave/src/App.jsx
+++ client/whisper-wave/src/App.jsx
@@ -7,6 +7,7 @@ const Home = lazy(() => import('./pages/Home'))
 const Login = lazy(() => import('./pages/Login'))
 const Chats = lazy(() => import('./pages/Chat'))
 const Groups = lazy(() => import('./pages/Groups'))
+const NotFound = lazy(() => import('./pages/NotFound'))
 
 let user = true;
 
@@ -24,6 +25,11 @@ function App() {
           <ProtectRoute user={!user} redirect='/'>
             <Login />
           </ProtectRoute>} />
+
+
+
+
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
